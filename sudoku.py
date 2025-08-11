7049aa6cc5fbc47c3280baa6e8eb7d98c6f65d7d
MariSalas23 <mari.masagu@gmail.com>
2025-08-11 02:22:56 -0500

    Competition Sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,59 +1,201 @@
-import numpy as np
-from ac import get_binarized_constraints_for_all_diff
+from collections import deque
+from pathless_tree_search import PathlessTreeSearch
 
-def get_bcn_for_sudoku(sudoku):
+def revise(bcn, X_i, X_j):
     """
-    Receives a Sudoku and creates a BCN definition from it, using the binarized All-Diff Constraint
+    Returns a tuple (D_i', changed), where
+        - D_i' is the maximal subset of the domain of X_i that is arc consistent with X_j
+        - changed is a boolean value that is True if the domain is now smaller than before and False otherwise
 
     Args:
-        sudoku (np.ndarray): numpy array describing the sudoku
+        bcn ((domains, constraints)): The BCN containting the constraints, in particular the one for X_i and X_j
+        X_i (Any): descriptor of the variable X_i
+        X_j (Any): descriptor of the variable X_j
+    """
+    domains, constraints = bcn
+    Di = domains[X_i]
+    Dj = domains[X_j]
+
+    key = tuple(sorted((X_i, X_j)))
+    if key not in constraints:
+        return Di[:], False
+
+    cons = constraints[key]
+    D_i_prime = []
+    for xi in Di:
+        # xi solo si existe algún xj en Dj que satisface la constraint
+        if any(cons({X_i: xi, X_j: xj}) for xj in Dj):
+            D_i_prime.append(xi)
+
+    changed = len(D_i_prime) != len(Di)
+    return D_i_prime, changed
+
+def ac3(bcn):
+    """
+    Reduces the domains in a BCN to make it arc consistent, if possible.
+
+    Args:
+        bcn ((domains, constraints)): The BCN to make arc consistent (if possible)
 
     Returns:
-        (domains, constraints): BCN describing the conditions for the given Sudoku
+        (bcn', feasible), where
+        - bcn' is the maximum subnetwork (in terms of domains) of bcn that is consistent
+        - feasible is a boolean that is False if it could be verified that bcn doesn't have a solution and True otherwise
     """
-    n = sudoku.shape[0]
-    values = list(range(1, n + 1))
+    domains0, constraints = bcn
+    domains = {} # Copia de los dominios
+    for v, d in domains0.items():
+        domains[v] = d[:] 
 
+    queue = deque() # Cola
+    # Inicializa cola con ambos sentidos de cada arco binario
+    for (a, b) in constraints.keys():
+        queue.append((a, b))
+        queue.append((b, a))
+
+    while queue:
+        Xi, Xj = queue.popleft()
+        D_i_prime, changed = revise((domains, constraints), Xi, Xj)
+        if changed:
+            domains[Xi] = D_i_prime
+            if len(D_i_prime) == 0:
+                return (domains, constraints), False  # Inconsistente
+            
+            for (a, b) in constraints.keys():
+                if a == Xi and b != Xj:
+                    queue.append((b, Xi))
+                elif b == Xi and a != Xj:
+                    queue.append((a, Xi))
+
+    return (domains, constraints), True # Returns (bcn', feasible)
+
+# ------------------------
+# Helpers para acelerar (no quitan tus comentarios de arriba)
+# ------------------------
+
+def _build_neighbors(constraints):
+    """Mapa de vecinos: para cada variable, quién comparte constraint con ella."""
+    neigh = {}
+    for (a, b) in constraints.keys():
+        if a not in neigh: neigh[a] = set()
+        if b not in neigh: neigh[b] = set()
+        neigh[a].add(b)
+        neigh[b].add(a)
+    return neigh
+
+def ac3_from(bcn, start_vars, neighbors): # Para la clasificación en board
+    domains0, constraints = bcn
     domains = {}
-    for r in range(n):
-        for c in range(n):
-            variable = (r, c)
-            value = int(sudoku[r, c])
-            if value == 0:
-                domains[variable] = values[:]
-            else:
-                domains[variable] = [value]
+    for v, d in domains0.items():
+        domains[v] = d[:]  # copia
 
-    constraints = {} 
-    def merge_constraints(dst, src): # Combina constraints
+    queue = deque()
+    # Inicializa la cola SOLO con arcos que entran a cada start_var
+    for X in start_vars:
+        for Y in neighbors.get(X, ()):
+            queue.append((Y, X))
 
-        for k, f in src.items():
-            if k in dst:
-                g = dst[k]
-                dst[k] = (lambda f1, f2: (lambda a: f1(a) and f2(a)))(g, f)
-            else:
-                dst[k] = f
+    while queue:
+        Xi, Xj = queue.popleft()
+        D_i_prime, changed = revise((domains, constraints), Xi, Xj)
+        if changed:
+            domains[Xi] = D_i_prime
+            if len(D_i_prime) == 0:
+                return (domains, constraints), False  # Inconsistente
+            for Xk in neighbors.get(Xi, ()):
+                if Xk != Xj:
+                    queue.append((Xk, Xi))
 
-    for r in range(n):
-        row_vars = {(r, c): domains[(r, c)] for c in range(n)}
-        cons_row = get_binarized_constraints_for_all_diff(row_vars)
-        merge_constraints(constraints, cons_row)
+    return (domains, constraints), True
 
-    for c in range(n):
-        col_vars = {(r, c): domains[(r, c)] for r in range(n)}
-        cons_col = get_binarized_constraints_for_all_diff(col_vars)
-        merge_constraints(constraints, cons_col)
+def get_tree_search_for_bcn(bcn, phi=None):
+    """
+        Generates a PathlessTreeSearch that can find a solution in the search space described by the BCN.
 
+    Args:
+        bcn ((domains, constraints)): The BCN in which we look for a solution.
+        phi (func, optional): Function that takes a dictionary of domains (variables are keys) and selects the variable to fix next.
 
-    b = int(np.sqrt(n))
-    assert b * b == n, "9x9, 16x16"
-    for br in range(0, n, b):
-        for bc in range(0, n, b):
-            block_vars = {}
-            for r in range(br, br + b):
-                for c in range(bc, bc + b):
-                    block_vars[(r, c)] = domains[(r, c)]
-            cons_block = get_binarized_constraints_for_all_diff(block_vars)
-            merge_constraints(constraints, cons_block)
+    Returns:
+        (search, decoder), where
+         - search is a PathlessTreeSearch object
+         - decoder is a function to decode a node to an assignment
+    """
+    (domains0, constraints) = bcn
+    (bcn_root, feasible) = ac3((domains0, constraints))
+    root_domains = bcn_root[0]
 
-    return domains, constraints # Returns (domains, constraints): BCN describing the conditions for the given Sudoku
+    if not feasible:
+        def _is_final(_): 
+            return False
+        return PathlessTreeSearch(n0=root_domains, succ=lambda _: [], goal=_is_final), (lambda _: {})
+
+    neighbors = _build_neighbors(constraints)
+    def is_final(dom):
+        for valores in dom.values():
+            if len(valores) != 1:
+                return False # Solución parcial
+        return True
+
+    def select_var(dom):
+        if phi is not None:
+            return phi(dom)
+        best_var, best_size, best_deg = None, 10**9, -1
+        for k, v in dom.items():
+            if len(v) > 1:
+                sz = len(v)
+                deg = len(neighbors.get(k, ()))
+                if (sz < best_size) or (sz == best_size and deg > best_deg):
+                    best_var, best_size, best_deg = k, sz, deg
+        return best_var
+
+    def succ(dom):
+        var = select_var(dom)
+        if var is None:
+            return []
+        children = []
+        for val in dom[var]:
+            new_dom = {k: vals[:] for k, vals in dom.items()}
+            new_dom[var] = [val]
+            (bcn2, feas2) = ac3_from((new_dom, constraints), start_vars=[var], neighbors=neighbors)
+            if feas2:
+                children.append(bcn2[0])
+        return children
+
+    search = PathlessTreeSearch(n0=root_domains, succ=succ, goal=is_final, better=None, order="bfs") # search is a PathlessTreeSearch object
+
+    def decoder(node): # decoder is a function to decode a node to an assignment
+        asignacion = {}
+        for var, dominio in node.items():
+            asignacion[var] = dominio[0] 
+        return asignacion
+
+    return search, decoder # Returns (search, decoder)
+
+def get_binarized_constraints_for_all_diff(domains):
+    """
+        Derives all binary constraints that are necessary to make sure that all variables given in `domains` will have different values.
+
+    Args:
+        domains (dict): dictionary where keys are variable names and values are lists of possible values for the respective variable.
+
+    Returns:
+        dict: dictionary where keys are constraint names (it is recommended to use tuples, with entries in the tuple being the variable names sorted lexicographically) and values are the functions encoding the respective constraint set membership
+    """
+    variables = sorted(domains.keys())
+    constraints = {} # dictionary where keys are constraint names (it is recommended to use tuples, with entries in the tuple being the variable names sorted lexicographically) and values are the functions encoding the respective constraint set membership
+
+    for i in range(len(variables)):
+        for j in range(i + 1, len(variables)):
+            a, b = variables[i], variables[j]
+
+            def make_neq(x, y):
+                def _neq(assign):
+                    if x in assign and y in assign:
+                        return assign[x] != assign[y]
+                    return True  # Asignación parcial
+                return _neq
+
+            constraints[(a, b)] = make_neq(a, b)
+
+    return constraints # Returns dict
